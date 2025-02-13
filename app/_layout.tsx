797d49f0cd087fdb74ed3b6d2cab5ad3f13460a9
Change Law <4575752+kinzlaw@users.noreply.github.com>
2025-02-13 12:25:22 +0700

    unknow modification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Home1' }} />
+      {/* <Stack.Screen name="about" options={{ title: 'About2' }} /> */}
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import * as Icons from "react-native-heroicons/micro"
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Home 
+        <Icons.SparklesIcon color='blue' /> 
+        screen
+      </Text>
+      <Icons.ArchiveBoxIcon color='red' />
+      <Link href="/about" style={styles.button}>
+        Goto About
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'grey',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'yellow',
+  },
+  button: {
+    fontSize: 20,
+    // textDecorationLine: 'underline',
+    color: 'white',
+  }
+})
